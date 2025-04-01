a3303844edc3147bcca48e50ec855ccba74333f1
Raghavendraprasad1 <raghavendrakumar1520@gmail.com>
2025-04-01 13:37:30 +0530

    changes to login page

--- resources/views/admin/login.blade.php
+++ resources/views/admin/login.blade.php
@@ -3,12 +3,12 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Admin Login</title>
+    <title>Admin Login1</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
 </head>
 <body>
     <div class="container mt-5">
-        <h2 class="text-center">Admin Login</h2>
+        <h2 class="text-center">Admin Login Page</h2>
         <form method="POST" action="{{ route('admin.login.submit') }}" style="width: 50%;
     margin-left: 270px;" >
             @csrf
